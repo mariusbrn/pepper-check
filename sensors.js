328d33ca5c7bf655ac5dcc72673d63dd85fa6ad7
Marius Baron <maruis.baron@gmail.com>
2017-10-14 14:03:43 +0200

    use adc MCp3008

--- sensors.js
+++ sensors.js
@@ -1,10 +1,19 @@
 const rpio = require('rpio');
+const Mcp3008 = require('mcp3008.js');
+
+const channel = 5;
+
+let adc;
 
 exports.init = function init() {
   console.log('Init sensors...');
-  rpio.open(11, rpio.INPUT);
+  rpio.spiBegin();
+  adc = new Mcp3008();
 };
 
 exports.check = function check() {
-  return { pin11: rpio.read(11) };
+  adc.read(channel, value => {
+    console.log('ADC check', value);
+  });
+  return 0;
 };
