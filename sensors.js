e36d26720d60db9825e3e03f9980e17120a9017a
Marius Baron <maruis.baron@gmail.com>
2017-10-15 12:45:46 +0200

    remove mcp3008.js

--- sensors.js
+++ sensors.js
@@ -1,19 +1,41 @@
 const rpio = require('rpio');
-const Mcp3008 = require('mcp3008.js');
 
-const channel = 5;
+function read(channel) {
+  const sendBuffer = new Buffer([0x01, (8 + channel) << 4, 0x01]);
+  const recieveBuffer = new Buffer(sendBuffer.length);
 
-let adc;
+  // Send TX buffer and recieve RX buffer
+  rpio.spiTransfer(sendBuffer, recieveBuffer, sendBuffer.length);
+
+  // Extract value from output buffer. Ignore first byte.
+  const [junk, MSB, LSB] = recieveBuffer;
+
+  // Ignore first six bits of MSB, bit shift MSB 8 positions and
+  // finally combine LSB and MSB to get a full 10 bit value
+  const value = ((MSB & 3) << 8) + LSB;
+
+  return value.toString();
+}
+
+function readPct(channel) {
+  const value = read(channel);
+
+  return `${100 - Math.round(value / 1023 * 100)}%`;
+}
 
 exports.init = function init() {
   console.log('Init sensors...');
+
   rpio.spiBegin();
-  adc = new Mcp3008();
+  //rpio.spiChipSelect(0);                  /* Use CE0 (slave 0) */
+  //rpio.spiSetCSPolarity(0, rpio.LOW);    /* Commonly chip enable (CE) pins are active low, and this is the default. */
+  //rpio.spiSetClockDivider(256);           /* MCP3008 max is ~1MHz, 256 == 0.98MHz */
+  //rpio.spiSetDataMode(0);
 };
 
 exports.check = function check() {
-  adc.read(channel, value => {
-    console.log('ADC check', value);
-  });
-  return 0;
+  return {
+    channel5: readPct(5),
+    channel6: readPct(6)
+  };
 };
--- spi_dump.js
+++ spi_dump.js
@@ -8,43 +8,42 @@ rpio.spiBegin();
 //rpio.spiSetDataMode(0);
 
 process.stdout.write('\x1b[36m');
-for (var channelHeader = 0; channelHeader <= 7; channelHeader++) {
+for (let channelHeader = 0; channelHeader <= 7; channelHeader++) {
     process.stdout.write(
-        'ch' +
-            channelHeader.toString() +
-            (channelHeader == 7 ? '\x1b[0m\n' : '\t')
+        `ch${channelHeader.toString()}${channelHeader === 7
+            ? '\x1b[0m\n'
+            : '\t'}`
     );
 }
 
-setInterval(function() {
-    for (var channel = 0; channel <= 7; channel++) {
+setInterval(() => {
+    for (let channel = 0; channel <= 7; channel++) {
         // Prepare TX buffer [trigger byte = 0x01] [channel 0 = 0x80 (128)] [placeholder = 0x01]
-        var sendBuffer = new Buffer([0x01, (8 + channel) << 4, 0x01]);
+        const sendBuffer = new Buffer([0x01, (8 + channel) << 4, 0x01]);
+        const recieveBuffer = new Buffer(sendBuffer.length);
 
-        var recieveBuffer = rpio.spiTransfer(sendBuffer, sendBuffer.length); // Send TX buffer and recieve RX buffer
+        rpio.spiTransfer(sendBuffer, recieveBuffer, sendBuffer.length); // Send TX buffer and recieve RX buffer
 
         // Extract value from output buffer. Ignore first byte.
-        var junk = recieveBuffer[0],
-            MSB = recieveBuffer[1],
-            LSB = recieveBuffer[2];
+        let [junk, MSB, LSB] = recieveBuffer;
 
         // Ignore first six bits of MSB, bit shift MSB 8 positions and
         // finally combine LSB and MSB to get a full 10 bit value
-        var value = ((MSB & 3) << 8) + LSB;
+        const value = ((MSB & 3) << 8) + LSB;
 
-        process.stdout.write(value.toString() + (channel == 7 ? '\n' : '\t'));
+        process.stdout.write(value.toString() + (channel === 7 ? '\n' : '\t'));
     }
 }, 1000);
 
-process.on('SIGTERM', function() {
+process.on('SIGTERM', () => {
     process.exit(0);
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', () => {
     process.exit(0);
 });
 
-process.on('exit', function() {
+process.on('exit', () => {
     console.log('\nShutting down, performing GPIO cleanup');
     rpio.spiEnd();
     process.exit(0);
